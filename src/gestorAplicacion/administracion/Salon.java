7978be5285498d772a56438b736f4244f6a7d14e
DavidPosadaSalazar <142468178+DavidPosadaSalazar@users.noreply.github.com>
2024-08-14 16:21:41 -0500

    Create Salon.java

---
+++ src/gestorAplicacion/administracion/Salon.java
@@ -0,0 +1,62 @@
+//Juan Diego / Daniel Hernando Zambrano Gonzales/ David Posada / Juan Miguel Ochoa Agudelo
+package gestorAplicacion.administracion;
+import java.util.ArrayList;
+import java.io.Serializable;
+
+public class Salon implements Serializable{
+        private String lugar;
+        private int aforo;
+        private Horario horario;
+        public static ArrayList<Salon> salones= new ArrayList<Salon>();
+        private static final long serialVersionUID = 1L;
+
+        public Salon(String lugar,int aforo){
+                this.lugar = lugar;
+                this.aforo = aforo;
+                this.horario = new Horario();
+                salones.add(this);
+        }
+        /* Este metodo se utiliza para hacer una enumeracion del lugar donde se tiene la clase */
+        public static String mostrarSalones() {
+                String retorno = "";
+                int i = 1;
+                for(Salon salon:salones) {
+                        retorno += (i++)+". "+salon.lugar+".\n";
+                }
+                return retorno;
+        }
+        /* Metodos set y get de la clase*/
+        
+        public String getLugar() {
+                return lugar;
+        }
+
+        public void setLugar(String lugar) {
+                this.lugar = lugar;
+        }
+
+        public int getAforo() {
+                return aforo;
+        }
+
+        public void setAforo(int aforo) {
+                this.aforo = aforo;
+        }
+
+        public Horario getHorario() {
+                return horario;
+        }
+
+        public void setHorario(Horario horario) {
+                this.horario = horario;
+        }
+
+        public static ArrayList<Salon> getSalones() {
+                return salones;
+        }
+
+        public static void setSalones(ArrayList<Salon> salones) {
+                Salon.salones = salones;
+        }
+    
+}
