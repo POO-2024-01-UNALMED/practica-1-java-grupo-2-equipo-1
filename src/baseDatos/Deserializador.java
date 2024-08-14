0d1aaf887b141cc064314b440776960dedd92cad
Diegosanchez100 <juasanchezmu@unal.edu.co>
2024-08-14 17:48:49 -0500

    Update Deserializador.java

--- src/baseDatos/Deserializador.java
+++ src/baseDatos/Deserializador.java
@@ -1,4 +1,4 @@
-//Juan Diego / Daniel Hernando Zambrano Gonzales/ David Posada / Juan Miguel Ochoa Agudelo
+//Juan Diego Sanchez / Daniel Hernando Zambrano Gonzales/ David Posada / Juan Miguel Ochoa Agudelo
 package baseDatos;
 
 import java.io.File;
